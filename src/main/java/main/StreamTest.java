64838a5691116daf454512c19aa0fc1c9ffd7d2a
jiaxin <897162029@qq.com>
2020-10-16 11:42:35 +0800

    代码提交

--- src/main/java/main/StreamTest.java
+++ src/main/java/main/StreamTest.java
@@ -17,6 +17,11 @@ public class StreamTest {
         List<String> list  = new ArrayList<>();
         //Stream<String> stream = list.stream();
         Stream<Integer> stream1 = Stream.iterate(0 ,(x) ->x+1 );
-        stream1.forEach(System.out::print);
+        stream1.limit(2).forEach(System.out::println);
+        List<Employees> employees = new ArrayList<>();
+        employees.add(new Employees(1,1,"zhangsan"));
+        employees.add(new Employees(2,1,"lisan"));
+        employees.add(new Employees(3,14,"zhangqia"));
+        employees.stream().map((e) ->e.getAge()).filter(e ->e>10).forEach(System.out::println);
     }
 }
